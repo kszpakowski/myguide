8471200c0bac4de8931c2f2d369e6f933e22a21e
Karol Szpakowski <karol.szpakowski@gmail.com>
2024-05-20 12:03:00 +0200

    initial restaurant page

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,72 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col">
+      <div className="wrap-breadcrumb pl-6">
+        <ol className='flex p-4'>
+          <li className="before:content-['>'] before:p-2">Karol's guide</li>
+          <li className="before:content-['>'] before:p-2">Poland</li>
+          <li className="before:content-['>'] before:p-2">Warsaw</li>
+          <li className="before:content-['>'] before:p-2">Restaurants</li>
+          <li className="before:content-['>'] before:p-2">Butchery & Wine</li>
+        </ol>
+      </div>
+      <div className='flex divide-x'>
+        <div>
+          <img src="https://axwwgrkdco.cloudimg.io/v7/__gmpics__/80df661e26ee438580db2398c3fa2709?w=600&amp;h=600&amp;org_if_sml=1" />
+        </div>
+        <div className=''>
+          <img src="https://axwwgrkdco.cloudimg.io/v7/__gmpics__/aafdeb461fbe428db0989f72ad898529?w=600&amp;h=600&amp;org_if_sml=1" />
+        </div>
+        <div className=''>
+          <img src="https://axwwgrkdco.cloudimg.io/v7/__gmpics__/68f6c253dfa84cfd81f43c5e30199b81?w=600&amp;h=600&amp;org_if_sml=1" />
         </div>
       </div>
+      <div className='mx-auto'>
+        <div className='p-10  max-w-screen-xl grid gap-12'>
+          <div>
+            <h1>Butchery & Wine</h1>
+            <ul className='text-sm'>
+              <li>
+                ul. Żurawia 22, Warsaw, 00 515, Polan
+              </li>
+              <li>
+                €€ · Meats and Grills, Traditional Cuisine
+              </li>
+            </ul>
+          </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div>
+            <h2>Karol Guide’s Point Of View</h2>
+            <p>The name of this busy modern bistro says it all; the emphasis is on steaks, which are dry-aged for a minimum of 28 days in their glass-fronted ageing fridges. The cuts are then skilfully cooked in the Bertha oven, where temperatures can reach up to 500°C. Try a Polish cut or one imported from further afield, accompanied with a glass of red wine. Non-steak dishes might include homemade black pudding and Kashubian style fish soup.</p>
+          </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div>
+            <h2>Rating</h2>
+            <div>
+              <p>Taste: 8/10</p>
+              <p>Look: 5/10</p>
+              <p>Atmosphere: 6/10</p>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div>
+            <h2>Audience</h2>
+            <ul>
+              <li>Friends</li>
+              <li>Coworkers</li>
+            </ul>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div>
+            <h2>Location</h2>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div>
+            <h2>Information</h2>
+          </div>
+
+        </div>
+
       </div>
     </main>
   );
